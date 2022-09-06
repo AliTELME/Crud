787d0709c23e9323f195db35686ad1c27f9b21e0
Ali <ali@ahmer.com>
2022-09-07 02:15:13 +0500

    Third Commit

--- project/settings.py
+++ project/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crud',
-        'USER' : 'pny',
-        'PASSWORD':'123456',
-        'HOST': 'localhost',
-        'PORT' : '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'crud',
+        # 'USER' : 'pny',
+        # 'PASSWORD':'123456',
+        # 'HOST': 'localhost',
+        # 'PORT' : '3306',
     }
 }
 
